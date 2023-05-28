49cf0048ee5a02685847768b5e523afc3b803385
Amarpreet <gillpreet1998@gmail.com>
2023-05-28 16:13:24 -0600

    Register User

--- backend-server/index.js
+++ backend-server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath} from "url";
+import { register } from "./controllers/auth.js";
 
 // Configuartions
 const __filename = fileURLToPath(import.meta.url);
@@ -34,7 +35,8 @@ const storage = multer.diskStorage({
 });
 const upload = multer({storage}); // help us to save it .. everytime we upload a file we will use 'upload' variable
 
-
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
    //mONGOOSE sETUP
 
 
--- models/user.js
+++ models/user.js
@@ -1,10 +1,46 @@
-const mongoose = require('mongoose')
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: {
+    firstName: {
         type: String,
-        required: true
-    }
-})
+        required: true,
+        min: 2,
+        max: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        min:2,
+        max: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        max: 50
+    },
+    password: {
+        type: String,
+        required: true,
+        min:5,
+        
+    },
+    pisturePath: {
+        type: String,
+       default: ""
+        
+    },
+    friends: {
+        type: Array,
+        default: []
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
+},
+{timestamps: true}
+);
 
-module.exports = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
+export default User;
