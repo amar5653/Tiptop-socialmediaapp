da51a6de95184bf5bbdd14a6e5f0d70e689d7208
Amarpreet <gillpreet1998@gmail.com>
2023-06-12 18:34:59 -0600

    Finishing project

--- backend-server/index.js
+++ backend-server/index.js
@@ -7,7 +7,7 @@ import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
-import { fileURLToPath} from "url";
+import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
@@ -16,31 +16,32 @@ import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
 import Post from "./models/Post.js";
-import { users, posts} from "./data/index.js";
-// Configuartions
+import { users, posts } from "./data/index.js";
+
+/* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname,'pulic/assets')));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-//File Storage
+/* FILE STORAGE */
 const storage = multer.diskStorage({
-    destination: function( req,file, cb ){  
-        cb(null, "public/assets"); /*everytime photo or video uploaded by user will be saved in public/assets */
-    },
-    filename: function(req, file, cb) {
-        cb(null, file.originalname);
-    }
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
-const upload = multer({storage}); // help us to save it .. everytime we upload a file we will use 'upload' variable
+const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
@@ -51,20 +52,19 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
-//mONGOOSE sETUP
-
-
-const PORT = process.env.PORT || 4001;
-const url = process.env.DATABASE_URL
-mongoose.connect(url, {
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+const url = process.env.DATABASE_URL;
+mongoose
+  .connect(url, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then (() => {
-    app.listen(PORT, () => {console.log(` Connected on Server Port: ${PORT}`)});
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Connected on Port: ${PORT}`));
 
-    /* Add data one time */
-  // User.insertMany(users);
-  // Post.insertMany(posts);
-})
-.catch((err) => {console.log(`${err} not connected`)})
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
